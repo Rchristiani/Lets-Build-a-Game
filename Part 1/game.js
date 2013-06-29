525689e38addeaa522a92a2d936d28f155da3fad
Ryan Christiani <ryan@ryanchristiani.com>
2013-06-28 23:14:29 -0400

    Adding content for part 1

--- Part 1/game.js
+++ Part 1/game.js
@@ -7,14 +7,39 @@ Game.canvas = document.getElementById('canvas');
 Game.ctx = Game.canvas.getContext('2d');
 
 //Set some constants
-Game.H = Game.canvas.height;
-Game.W = Game.canvas.width;
+Game.H = 600;
+Game.W = 800;
+//Set the height and with of the canvas
+Game.canvas.height = Game.H;
+Game.canvas.width = Game.W;
 
 Game.update = function() {
 	//Lets paint the background
 	//Set the fill style to be one of those not quite black but cool colors
 	Game.ctx.fillStyle = '#273636';
+	//Paint the background 
 	Game.ctx.fillRect(0,0,Game.W,Game.H);
+	//Paint the player
+	Game.player.paint();
+
+	requestAnimationFrame(Game.update);
 };
 
-Game.update();
+//Player Object 
+Game.player = {
+	x: 0,
+	y: 0,
+	w: 64,
+	h: 64,
+	//Add an image
+	img: new Image(),
+	paint: function() {
+		//Set the src
+		this.img.src = 'imgs/player_ship.gif';
+		//Draw the image
+		Game.ctx.drawImage(this.img,this.x,this.y,this.w,this.h);
+	}
+}
+
+//Call the update function
+requestAnimationFrame(Game.update);
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 #Lets Build a Game
 
+##Part 1
+In part 1 we will get the core structure of your code down, and get drawing to the screen as well as set up the game loop.
+
+###Assets
+
+Any art assests are taken from [here](http://opengameart.org/content/spaceships-1)
+
